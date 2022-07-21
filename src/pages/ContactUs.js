c0f577444493d4764d987a15de354c3016361b16
Kei En <njuguna0karanja@gmail.com>
2022-07-21 18:38:45 +0300

    Contact animation

--- src/pages/ContactUs.js
+++ src/pages/ContactUs.js
@@ -1,19 +1,69 @@
 import React from "react";
 //Animations
 import { motion } from "framer-motion";
-import { pageAnimation } from "../animation";
+import { pageAnimation , titleAnim} from "../animation";
+import styled from "styled-components";
 
 const ContactUs = () => {
     return (
-        <motion.div
+        <ContactStyle
             variants={pageAnimation}
             initial="hidden"
             animate="show"
             exit="exit"
         >
-            <h1>Our Contact</h1>
-        </motion.div>
+            <Title>
+                <Hide>
+                    <motion.h2 variants={titleAnim}>Get in touch.</motion.h2>
+                </Hide>
+            </Title>
+            <Hide>                
+                <Social variants={titleAnim}>
+                    <Circle />
+                    <h2>Send us a message</h2>
+                </Social>
+            </Hide>
+            <Hide>                
+                <Social variants={titleAnim}>
+                    <Circle />
+                    <h2>Contact information</h2>
+                </Social>
+            </Hide>
+            <Hide>                
+                <Social variants={titleAnim}>
+                    <Circle />
+                    <h2>Social media pages</h2>
+                </Social>
+            </Hide>
+        </ContactStyle>
     )
 }
+
+//Styles
+const ContactStyle = styled(motion.div)`
+    padding: 3rem 10rem;
+    color: white;
+    min-height: 90vh;
+    background: #1b1b1b;
+`;
+const Title = styled.div`
+    margin-bottom: 4rem;
+`;
+const Hide = styled.div`
+    overflow: hidden;
+`;
+const Circle = styled.div`
+    border-radius: 50%;
+    width: 3rem;
+    height: 3rem;
+    background: #dddddd;
+`;
+const Social = styled(motion.div)`
+    display: flex;
+    align-items: center;
+    h2 {
+        margin: 2rem;
+    }
+`;
 
 export default ContactUs;
